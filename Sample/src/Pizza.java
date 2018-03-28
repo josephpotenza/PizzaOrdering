ad251871cea6899baa25f468f7cf186b352838da
Victor <russovictor96@gmail.com>
2018-03-28 01:44:27 -0400

    Practice Gui

--- Sample/src/Pizza.java
+++ Sample/src/Pizza.java
@@ -1,4 +1,25 @@
-public class Pizza {
-   // this is a class pizza. no data yet.
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Pizza extends Application{
+
+    public void start(Stage primaryStage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/GuiFX.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
 }
 

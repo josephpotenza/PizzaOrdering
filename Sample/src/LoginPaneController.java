75929345f1ae9ab97ac8d12b3559ed43c03eef0e
Victor <russovictor96@gmail.com>
2018-04-01 01:49:50 -0400

    Finally Learned how to make a label clickable, also did some decorations.

--- Sample/src/LoginPaneController.java
+++ Sample/src/LoginPaneController.java
@@ -1,10 +1,16 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class LoginPaneController {
 
@@ -24,19 +30,32 @@ public class LoginPaneController {
     private PasswordField loginPassword;
 
     @FXML
+    private Label failedLoginLabel;
+
+    @FXML
     void submitLogin(ActionEvent event) {
-
+        //if login successful go to orderingPane
+        //else update failedLoginLabel
+        failedLoginLabel.setText("No User Found.");
     }
 
 
     @FXML
-    void switchToSignUpScene(ActionEvent event) {
-
+    void switchToSignUpScene(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("SignUpPane.fxml"));
+            Stage stage = (Stage) loginButton.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 
     @FXML
-    void switchToGuestLogin(ActionEvent event) {
+    void switchToGuestLogin(MouseEvent event) {
 
     }
 

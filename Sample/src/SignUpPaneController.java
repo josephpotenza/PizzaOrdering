cb920148a20e4eca603b423a87bf2cb33a82deb1
Victor <russovictor96@gmail.com>
2018-04-01 02:01:02 -0400

    Finally Learned how to make a label clickable, also did some decorations.

--- Sample/src/SignUpPaneController.java
+++ Sample/src/SignUpPaneController.java
@@ -1,9 +1,15 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class SignUpPaneController {
 
@@ -26,8 +32,24 @@ public class SignUpPaneController {
     private TextField firstNameInput;
 
     @FXML
+    private Label goBackButton;
+
+    @FXML
     void signUpNewCustomer(ActionEvent event) {
 
+    }
+
+    @FXML
+    void backToLoginPane(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("LoginPane.fxml"));
+            Stage stage = (Stage) emailInput.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }

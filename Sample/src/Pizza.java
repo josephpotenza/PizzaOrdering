d9febdb677586dc3e969de1bdbbd28afdf08940c
Victor <russovictor96@gmail.com>
2018-03-31 20:46:27 -0400

    Extras for GUi

--- Sample/src/Pizza.java
+++ Sample/src/Pizza.java
@@ -19,7 +19,7 @@ public class Pizza extends Application{
 
     public void start(Stage primaryStage){
         try{
-            Parent root = FXMLLoader.load(getClass().getResource("/GuiFX.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/LoginPane.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();

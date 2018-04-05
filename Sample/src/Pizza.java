be3742e4ac1036b5d1b9d4ee10f4318e44792d3c
christianposada <christian10305@gmail.com>
2018-04-05 18:54:17 -0400

    Updated code

--- Sample/src/Pizza.java
+++ Sample/src/Pizza.java
@@ -18,9 +18,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
-public class Pizza extends Application{
-
-    public void start(Stage primaryStage){
+public class Pizza {//extends Application{
+    static Scanner reader;
+    /*public void start(Stage primaryStage){
         try{
             Rectangle2D screenSize = Screen.getPrimary().getVisualBounds();
             Parent root = FXMLLoader.load(getClass().getResource("/LoginPane.fxml"));
@@ -35,56 +35,32 @@ public class Pizza extends Application{
 
         primaryStage.setMaximized(true);
         primaryStage.setResizable(true);
-    }
+    }*/
 
 
+    public static void main(String[] args) {
+        reader = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println("1. Customer 2. createAccount");
+            choice = reader.nextInt();
+            if (choice == 1) {
+                Customer customer = new Customer();
+                customer.login();
 
-    public static void main(String[] args){
-        launch(args);
-        /*Scanner read = new Scanner(System.in);
-        System.out.print("First Name:");
-        String fn;
-        fn = read.next();
-        System.out.print("Last Name:");
-        String ln;
-        ln = read.next();
-        System.out.print("Phone #");
-        String pn;
-        pn = read.next();
-        System.out.print("Email");
-        String em;
-        em = read.next();
-*/
-        // this is a working extraction method to pull info from customers table
-        // your welcome -Joe :)
-        try {
-            String host = "jdbc:mysql://18.221.69.113:3306/pizzaOrdering";
-            String uName = "root";
-            String uPass = "csc430db";
-            Connection con = DriverManager.getConnection(host, uName, uPass);
-            Statement stmt = con.createStatement( );
-            // this is a working insertion method to insert user input to db.
-          //  String SQL = "Insert INTO customers(LastName, FirstName, phone, email) VALUES ('"+ln+"','"+fn+"','"+pn+"','"+em+"')";
-            //stmt.executeUpdate(SQL);
-            String SQL = "SELECT * FROM customers";
-            ResultSet rs = stmt.executeQuery(SQL);
-
-            while ( rs.next( ) ) {
-
-                int id_col = rs.getInt("customerID");
-                String first_name = rs.getString("FirstName");
-                String last_name = rs.getString("LastName");
-                String phone = rs.getString( "phone");
-                String email = rs.getString( "email");
-                System.out.println("Customer ID: " + id_col + " Name: " + first_name + " " +last_name);
-                System.out.println("Email: " + email + " Phone: "+ phone);
+            }else{
+                Customer customer = new Customer();
+                customer.createAccount();
             }
-        }
-        catch (SQLException err) {
-            System.out.println(err.getMessage());
-        }
+
+
+        }while (choice != 3) ;
 
 
     }
 }
+
+
+
+
 

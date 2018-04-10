e361d99f3bf49f7c5b312bc6fcbdfb5e3a17989e
Victor <russovictor96@gmail.com>
2018-04-10 13:38:46 -0400

    Fixed Text Alignment

--- Sample/src/SignUpPaneController.java
+++ Sample/src/SignUpPaneController.java
@@ -4,10 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
@@ -39,17 +36,30 @@ public class SignUpPaneController {
     private Label goBackButton;
 
     @FXML
+    private Label successLabel;
+
+    @FXML
     void signUpNewCustomer(ActionEvent event) {
         Database temp = new Database();
         temp.connect();
 
         String SQL = "Insert INTO customers(LastName, FirstName, password, email) VALUES ('" + lastNameInput.getText() + "','" + firstNameInput.getText() + "','" + passwordInput.getText() + "','" + emailInput.getText() + "')";
-        try {
-            temp.statement.executeUpdate(SQL);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        if(lastNameInput.getText().trim().isEmpty() || firstNameInput.getText().trim().isEmpty() || passwordInput.getText().trim().isEmpty() || emailInput.getText().trim().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setContentText("Missing Required Fields.");
 
+            alert.showAndWait();
+        }
+        else{
+            try {
+                temp.statement.executeUpdate(SQL);
+                successLabel.setText("Success!");
+                 goBackButton.setText("Go Back To Login.");
+               } catch (SQLException e) {
+                 e.printStackTrace();
+            }
+        }
 
     }
 

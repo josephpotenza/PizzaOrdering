307de90920f2d5b125027d1343a542b52d2f2ce3
joe <josephpotenza@live.com>
2018-04-10 19:39:47 -0400

    insertion / exaction methods connected to gui bug fixes error checking

--- Sample/src/Pizza.java
+++ Sample/src/Pizza.java
@@ -40,7 +40,7 @@ public class Pizza extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("/LoginPane.fxml"));
             Scene scene = new Scene(root, 1600, 900);
             primaryStage.setScene(scene);
-            primaryStage.setTitle("The Best Project");
+            primaryStage.setTitle("Victor is gay");
             primaryStage.show();
         }
         catch (Exception e) {
